f4822e58316e9745615b477c805dfd51cde06390
CoekCx <nemanja.ja.jovicic7@gmail.com>
2022-12-15 16:52:29 +0100

    implemented error handlers, and started implementing swagger

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import json
 
 import jsonpickle
 from flask import request
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from app import app, db
 from models.user import User
 from models.transactions import Deposit, Transfer, Send
 from models.wallet import Wallet
 from models.card import valid_card
-
-BAD_RESPONSE = app.response_class(response=jsonpickle.encode({}),
-                                  status=401,
-                                  mimetype='application/json')
 
 try:
     with app.app_context():
@@ -27,6 +25,72 @@ def login():
     response = app.response_class(response=jsonpickle.encode(data),
                                   status=200,
                                   mimetype='application/json')
+
+    return response
+
+
+# <editor-fold desc="Swagger">
+
+
+# Add a route for serving the Swagger UI
+@app.route("/swagger")
+def swagger_ui():
+    return swagger(app)
+
+
+# Add a route for serving the Swagger UI
+@app.route("/docs")
+def docs():
+    # Create a blueprint for rendering the Swagger UI
+    swagger_ui_blueprint = get_swaggerui_blueprint(
+        "/swagger",
+        "/swagger.json",
+        config={
+            "app_name": "My Flask App"
+        }
+    )
+    return swagger_ui_blueprint
+
+
+# </editor-fold>
+
+
+# <editor-fold desc="Error Handlers">
+
+
+@app.errorhandler(400)
+def handle_400_error(_error):
+    """Return a http 400 error to client"""
+    return app.response_class(response=jsonpickle.encode({'error': 'Bad Request'}),
+                              status=400,
+                              mimetype='application/json')
+
+
+@app.errorhandler(401)
+def handle_401_error(_error):
+    """Return a http 401 error to client"""
+    return app.response_class(response=jsonpickle.encode({'error': 'Unauthorised'}),
+                              status=401,
+                              mimetype='application/json')
+
+
+@app.errorhandler(404)
+def handle_404_error(_error):
+    """Return a http 404 error to client"""
+    return app.response_class(response=jsonpickle.encode({'error': 'Not Found'}),
+                              status=404,
+                              mimetype='application/json')
+
+
+@app.errorhandler(500)
+def handle_500_error(_error):
+    """Return a http 500 error to client"""
+    return app.response_class(response=jsonpickle.encode({'error': 'Internal Server Error'}),
+                              status=500,
+                              mimetype='application/json')
+
+
+# </editor-fold>
 
 
 # <editor-fold desc="Utility">
@@ -58,6 +122,4 @@ def parse_form_data(form_data) -> dict:
 # </editor-fold>
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run()
